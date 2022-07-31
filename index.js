62f97ae5c3458919ea48575f3f75ef861ff7d143
tuantrollvanta <100056139+tuantrollvanta@users.noreply.github.com>
2022-07-31 10:00:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function drawTriangle(t){
+
+    for (let i=1; i <= t; i++)
+    {
+      let eachLine = ''
+    
+        for (let t=1; )
+        {
+          eachLine += j + " "
+        }
+        eachLine = eachLine.trim();
+        console.log(eachLine);
+    }
+    
+    }
+    
+    let t = 5 ;
+    drawTriangle(t);2
